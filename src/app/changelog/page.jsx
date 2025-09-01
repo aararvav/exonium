e23e7d46f07ad8af4160cb7dd6d0e2c60a528f32
aararvav <theaarav14@gmail.com>
2025-09-01 10:09:22 +0530

    updated navbar

--- src/app/changelog/page.jsx
+++ src/app/changelog/page.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Nav from "@/components/nav/nav";
+
 
 export default function ChangelogPage() {
   return (
     <main className="min-h-screen flex items-center justify-center">
+      <Nav />
       <h1 className="text-4xl font-bold">Changelog Page</h1>
     </main>
   );
--- src/app/features/page.jsx
+++ src/app/features/page.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Nav from "@/components/nav/nav";
+
 
 export default function FeaturesPage() {
   return (
     <main className="min-h-screen flex items-center justify-center">
+      <Nav />
       <h1 className="text-4xl font-bold">Features Page</h1>
     </main>
   );
